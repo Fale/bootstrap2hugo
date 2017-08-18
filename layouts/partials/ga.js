02e42d5c039c41b932eb7d9fe484f3663a4d6f45
Fabio Alessandro Locati <me@fale.io>
2017-08-18 16:28:35 +0200

    Bootstrap4

--- layouts/partials/ga.js
+++ layouts/partials/ga.js
@@ -1,24 +1,7 @@
 <script>
-(function() {
-  if (window.location.hostname === "localhost") {
-    console.log("Local dev, so no Google Analytics.");
-    return;
-  } else {
-    (function(i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', '{{ .Site.GoogleAnalytics }}', 'auto');
-    ga('set', 'anonymizeIp', true);
-    ga('send', 'pageview');
-  }
-})();
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', '{{ .Site.GoogleAnalytics }}', 'auto');
+ga('set', 'anonymizeIp', true);
+ga('send', 'pageview');
 </script>
+<script async src='https://www.google-analytics.com/analytics.js'></script>
