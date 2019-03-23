21e8c7efc0508c501c293fb11c84d5b5dfcf1550
Fabio Alessandro Locati <me@fale.io>
2019-03-23 10:34:37 +0100

    more generic code

--- layouts/partials/ga.js
+++ layouts/partials/ga.js
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-62427039-3"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ .Site.GoogleAnalytics }}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
