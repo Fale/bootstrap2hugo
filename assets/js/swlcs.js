c552b2422c684dea10ea9c292ff5b95d2f691670
Fabio Alessandro Locati <me@fale.io>
2020-07-18 13:23:42 +0200

    improve commenting UX

--- assets/js/swlcs.js
+++ assets/js/swlcs.js
@@ -5,13 +5,11 @@ $(function() {
             type: "POST",
             url: "{{ .Site.Params.Swlcs }}",
             dataType: 'json',
-            //data: formToJson($("form#comment-form")),
             data: JSON.stringify( { "resource": $('#resource').val(), "name": $('#name').val(), "email": $('#email').val(), "body": $('#body').val() } ),
             contentType: 'application/json;charset=UTF-8',
             success : function(result) {
-                // you can see the result from the console
-                // tab of the developer tools
-                console.log(result);
+                $("form#comment-form").hide();
+                $( "section#comments" ).prepend( "<p>Thanks a lot for your comment! If the moderation is enabled, the comment has been added to the moderation queue. Otherwise, it will appear in a few minutes on the page.</p>" );
             },
             error: function(xhr, resp, text) {
                 console.log(xhr, resp, text);
